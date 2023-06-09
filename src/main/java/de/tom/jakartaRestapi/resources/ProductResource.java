35e47828e8a5fd02cfc49acb54d7b4bbbcfd157f
Tom L <devzemux1613@gmail.com>
2023-06-09 19:45:46 +0200

    refactor: modify product endpoints

--- src/main/java/de/tom/jakartaRestapi/resources/ProductResource.java
+++ src/main/java/de/tom/jakartaRestapi/resources/ProductResource.java
@@ -7,12 +7,14 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Optional;
 
 @Path("/api")
 public class ProductResource {
 
     @GET
+    @Produces(MediaType.TEXT_PLAIN)
     @Path("/product/{productName}")
     public Product getData(@PathParam("productName") String productName) {
         return ProductRepositoryInMemory
@@ -21,17 +23,16 @@ public class ProductResource {
                 .stream()
                 .filter(product -> product.getName().equals(productName))
                 .findFirst()
-                .orElse(null);
+                .orElse(new Product("", 0, Collections.EMPTY_LIST));
     }
 
     @PUT
     @Path(("/product/{productName}"))
-    public void deleteProduct(@PathParam("productName") String productName) {
+    public Response deleteProduct(@PathParam("productName") String productName) {
         ProductRepositoryInMemory
                 .getInstance()
-                .remove(
-                        getData(productName)
-                );
+                .remove(getData(productName));
+        return Response.ok("delete " + getData(productName)).build();
     }
 
     @POST
